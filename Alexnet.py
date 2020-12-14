cbd4f26492c39c7e74ef4df832998fe51f5d1cf3
Roberto Bressani <roberto.bressani97@gmail.com>
2020-12-14 11:37:12 +0100

    Update Alexnet.py
    fix: copy of params failed due to wrong dimensions

--- Alexnet.py
+++ Alexnet.py
@@ -46,7 +46,7 @@ class AlexNet(nn.Module):
                                     nn.Dropout(),
                                     nn.Linear(4096, 4096),
                                     nn.ReLU(inplace=True),
-                                    nn.Linear(4096, 2), )
+                                    nn.Linear(4096, num_classes), )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.features(x)
